fc34bf954ade0ee4c7c6a0d7124170f996c93ec1
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-15 09:52:09 +0000

    swap two integers function

---
+++ swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: NONE
+ */
+ void swap(int *a, int *b)
+ {
+     int tmp;
+
+     tmp = *b;
+     *b = *a;
+     *a = tmp;
+ }
