1cb707972ccc243cc7c21c5e9be7e8e3935774c8
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-18 03:16:31 +0000

    modify selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,36 @@
 #include "sort.h"
 /**
- * swap - Function that swaps two values
- *
- * @a: Fisrt value
- * @b: Second value
+ * selection_sort - Function that sort array of integers
+ * Description: sort array using selection sort
+ * @array: array of integers
+ * @size: size of array
  * Return: NONE
  */
-void swap(int *a, int *b)
-{
-    int tmp;
-
-    tmp = *b;
-    *b = *a;
-    *a = tmp;
-}
 
 void selection_sort(int *array, size_t size)
 {
-    /* loop over all elements*/
-    size_t i;
-    size_t j;
-    for (i = 0; i < size; i++)
-    {
-        /* search for the smallest elements*/
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j]  < array[i])
-            {
-                swap(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
+	/* loop over all elements*/
+	size_t i;
+	size_t j;
+	int min;
+	int min_index;
+
+	for (i = 0; i < size; i++)
+
+	{
+		min = array[i];
+		min_index = i;
+		/* search for the smallest elements*/
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				min_index = j;
+			}
+		}
+		swap(&array[i], &array[min_index]);
+		print_array(array, size);
+	}
 }
+
